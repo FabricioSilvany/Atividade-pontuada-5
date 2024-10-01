28bcfce8c54e1a36077331acfbde202a1e1bfac9
FabricioSilvany <fabriciosilvany57@gmail.com>
2024-10-01 17:07:16 +0000

    Salvo enunciado

--- Fabrício.py
+++ Fabrício.py
@@ -1,3 +1,24 @@
 import os
+from dataclasses import dataclass
 
 os.system("cls || clear")
+
+
+Lista_familia = []
+
+#Perguntas
+@dataclass
+class Familia:
+    nome: str
+    idade: int
+    salario: float
+    numero_fiilhos: int
+
+#Solicitando dados
+familia = Familia(
+    nome = input("Digite seu nome: "),
+    idade = int(input("Digite sua idade: ")),
+    numero_fiilhos = int(input("Digite quantos filhos você tem: ")),
+    salario = float(input("Digite qual o seu salário: "))
+)
+Lista_familia.append(familia)
