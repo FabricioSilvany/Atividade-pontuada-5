fc1d0d777150d522f9c1d8535765a9e350e04351
FabricioSilvany <fabriciosilvany57@gmail.com>
2024-10-01 19:08:24 +0000

    Socorro

--- Codigo.py
+++ Codigo.py
@@ -13,58 +13,55 @@ quantidade_de_filhos = []
 @dataclass
 class Familia:
     sobrenome: str
-    numero_fiilhos: int
-
+    numero_filhos: int
 
 #Definido banco de dados
 nome_do_arquivo = "pesquisa_prefeitura.txt"
 
-#Abrindo arquivo e certificando a escrita de dados
-with open(nome_do_arquivo, "a") as arquivo_familias:
-    for familia in lista_familia:
-        arquivo_familias.write(f"{familia.numero_filhos, {familia.sobrenome}}")
-
-#Acessando dados do arquivo:
-with open(nome_do_arquivo, "r") as arquivo_de_origem:
-    for linha in arquivo_de_origem:
-        sobrenome, numero_filhos = linha.strip().split(",")
-        lista_familia.append(Familia(sobrenome = sobrenome, numero_filhos = int(numero_filhos)))
-
-#Fechando conexão com o arquivo
-arquivo_familias.close()
-
 #Menu
 while True:
-    opcao = input(print("""
+    opcao = int(input("""
     === PESQUISA DA PREFEITURA ===
     (Digite o número para acessar a aba desejada)
         
     1 || Adicionar família
     2 || Exibir resultados
     3 || Sair
-
-   :
     """))
+    os.system("cls || clear")
 
     match (opcao):
         case 1:
             familia = Familia(
             sobrenome = input("Digite o sobrenome da família: "),
-            numero_fiilhos = int(input("Digite quantos filhos você tem: "))
+            numero_filhos = int(input("Digite quantos filhos você tem: ")),
             )
+            os.system("cls || clear")
+
             lista_familia.append(familia)
 
             quantidade_familias += 1
 
-            print("Dados da família salvos com sucesso.")
-            #Fechando conexão com o arquivo
-            arquivo_familias.close()
+            print("\nDados da família salvos com sucesso.")
+
+            #Abrindo arquivo e certificando a escrita de dados
+            with open(nome_do_arquivo, "a") as arquivo_familias:
+                for integrante in lista_familia:
+                    arquivo_familias.write(f"{familia.sobrenome}, {familia.numero_filhos}\n")
 
         case 2:
+            #Lendo o arquivo
+            with open(nome_do_arquivo, "r") as arquivo_de_origem:
+                for linha in arquivo_de_origem:
+                    sobrenome, numero_filhos = linha.strip().split(",")
+                    lista_familia.append(Familia(sobrenome = sobrenome, numero_filhos = int(numero_filhos)))
+
             for parente in lista_familia:
-                print(f"Sobrenome das famílias: {sobrenome} ")
+                print(f"Sobrenome da família: {sobrenome}")
+                print(f"Média de filhos das famílias: {sum(quantidade_de_filhos) / len(quantidade_de_filhos)}")
 
         case 3:
             break
+
         case _:
             print("Aba invalida ou inexistente. \nTente novamente.")
