0345565691e958b1bb272dd1fdfaaededc0d15e8
Iron-Holster <victorandradecostapinto@gmail.com>
2024-10-01 17:06:07 +0000

    SAVE

--- Victor.py
+++ Victor.py
@@ -1,2 +1,26 @@
 import os
 os.system("cls || clear") 
+
+quantidade_familias = 0
+
+opcao = input(print("""
+=== PESQUISA DA PREFEITURA ===
+(Digite o número para acessar a aba desejada)
+      
+1 || Adicionar família
+2 || Exibir resultados
+3 || Sair
+
+:
+"""))
+
+match (opcao):
+    case 1:
+        quantidade_familias += 1
+        
+    case 2:
+
+    case 3:
+        break
+    case _:
+        print("Aba invalida ou inexistente. \nTente novamente.")
